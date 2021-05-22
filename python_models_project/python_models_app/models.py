bc87b9ec7b024d7de6f05b1fe78d5571dbb76996
digipa <37922236+digipa@users.noreply.github.com>
2021-05-21 21:00:53 -0400

    Created a model with classes that return fields as strings in file named models.py.

---
+++ python_models_project/python_models_app/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Faculty(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Department(models.Model):
+    faculty = models.ForeignKey(Faculty, on_delete= models.CASCADE)
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class School(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Certificate_type(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Grade(models.Model):
+    type = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.type
+
+class Student(models.Model):
+    department = models.ForeignKey(Department, default="", on_delete = models.CASCADE)
+    grade = models.ForeignKey(Grade, on_delete= models.CASCADE)
+    school = models.ForeignKey(School, on_delete= models.CASCADE)
+    certificate_type = models.ForeignKey(Certificate_type, on_delete= models.CASCADE)
+    fullname = models.CharField(max_length=30)
+    year_of_graduation = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.fullname, self.year_of_graduation
